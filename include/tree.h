7179e1f03f08f23ee56616986cd4610420179ed6
Рыбина Екатерина <rybinae58@gmail.com>
2026-06-08 15:27:44 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -2,4 +2,35 @@
 #ifndef INCLUDE_TREE_H_
 #define INCLUDE_TREE_H_
 
+#include <vector>
+#include <memory>
+#include <algorithm>
+
+class PMTree {
+private:
+  struct N {
+    char val;
+    std::vector<std::shared_ptr<N>> children;
+    N(char v) : val(v) {}
+  };
+  std::shared_ptr<N> root;
+  std::vector<char> elements;
+  int totalPermutations;
+  void buildTree(std::shared_ptr<N> node, std::vector<char> remaining);
+  void getAllPermutationsDFS(std::shared_ptr<N> node, std::vector<char>& current, 
+                               std::vector<std::vector<char>>& result);
+  int getSubtreePermutations(std::shared_ptr<N> node);
+
+public:
+  PMTree(const std::vector<char>& input);
+  friend std::vector<std::vector<char>> getAllPerms(PMTree& tree);
+  friend std::vector<char> getPerm1(PMTree& tree, int num);
+  friend std::vector<char> getPerm2(PMTree& tree, int num);
+  int getTotalPermutations() const { return totalPermutations; }
+};
+
+std::vector<std::vector<char>> getAllPerms(PMTree& tree);
+std::vector<char> getPerm1(PMTree& tree, int num);
+std::vector<char> getPerm2(PMTree& tree, int num);
+
 #endif  // INCLUDE_TREE_H_
